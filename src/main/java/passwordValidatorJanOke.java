30733d3d717604408fe26a8b77edf15c6155e8ba
Jan-Oke Mettendorf <jan-oke-mettendorf@web.de>
2021-09-10 15:54:47 +0200

    improve test code

--- src/main/java/passwordValidatorJanOke.java
+++ src/main/java/passwordValidatorJanOke.java
@@ -1,5 +1,142 @@
 public class passwordValidatorJanOke {
 
-    //comment
+    public static void main(String[] args){
+
+        String password = "kwjgFöl";
+        String[] passwordList = {"npasdDasddnpo","jhasdDasdadsdegio","kaoDenasdasdagop"};
+
+        System.out.println(checkPasswordLength(password));
+        System.out.println(checkPasswordContainsDigits(password));
+        System.out.println(checkPasswordContainsUpperOrLowerChars(password));
+        System.out.println(checkPasswordListLengthValid(passwordList));
+        System.out.println(checkPasswordListContainsDigits(passwordList));
+        System.out.println(checkPasswordListContainsUpperOrLowerChars(passwordList));
+
+    }
+
+    public static String checkPasswordLength(String password){
+
+        int passwordLen = password.length();
+
+        if (passwordLen < 8){
+
+            return "Password is too short";
+
+        }
+
+        return "Password is valid";
+
+    }
+
+    public static boolean checkPasswordContainsDigits(String password){
+
+        return password.matches(".*\\d.*");
+
+    }
+
+    public static boolean checkPasswordContainsUpperOrLowerChars(String password){
+
+        int upperCaseCount = 0;
+        int lowerCaseCount = 0;
+
+        for (int i = 0; i < password.length(); i++){
+
+            char passwordChar = password.charAt(i);
+
+            if (Character.isUpperCase(passwordChar))
+                upperCaseCount++;
+            if (Character.isLowerCase(passwordChar))
+                lowerCaseCount++;
+
+        }
+
+        return (upperCaseCount > 0 && lowerCaseCount > 0);
+
+    }
+
+    public static String checkPasswordListLengthValid(String[] passwordList){
+
+        for (int i = 0; i < passwordList.length; i++){
+
+            int passwordLen = passwordList[i].length();
+
+            if (passwordLen < 8){
+
+                return "One or more of the passwords is too short";
+
+            }
+
+        }   return "Passwords are valid";
+
+    }
+
+    public static String checkPasswordListContainsDigits(String[] passwordList){
+
+        int digitCount = 0;
+
+        for (int i = 0; i < passwordList.length; i++){
+
+            if (passwordList[i].matches(".*\\d.*")) {
+
+                digitCount++;
+
+            }
+
+        }
+
+        if (digitCount == 1 ) {
+
+            return digitCount + " password contains digits.";
+
+        } else if (digitCount > 1) {
+
+            return digitCount + " passwords contain digits.";
+
+        }
+
+        return "no password contains digits.";
+
+    }
+
+    public static String checkPasswordListContainsUpperOrLowerChars(String[] passwordList){
+
+        int upperAndLowerCaseCount = 0;
+        int upperCaseCount = 0;
+        int lowerCaseCount = 0;
+
+        for (int i = 0; i < passwordList.length; i++){
+
+            for (int y = 0; y < passwordList[i].length(); y++) {
+
+                char passwordChar = passwordList[i].charAt(y);
+
+                if (Character.isUpperCase(passwordChar))
+                    upperCaseCount++;
+                if (Character.isLowerCase(passwordChar))
+                    lowerCaseCount++;
+
+            }
+
+            if (upperCaseCount > 0 && lowerCaseCount > 0)
+                upperAndLowerCaseCount++;
+
+            upperCaseCount = 0;
+            lowerCaseCount = 0;
+
+        }
+
+        if (upperAndLowerCaseCount == 1 ) {
+
+            return upperAndLowerCaseCount + " password contains lower and upper case letters.";
+
+        } else if (upperAndLowerCaseCount > 1) {
+
+            return upperAndLowerCaseCount + " passwords contain lower and upper case letters.";
+
+        }
+
+        return "no password contains lower and upper case letters.";
+
+    }
 
 }
--- src/test/java/TestPasswordValidatorJanOke.java
+++ src/test/java/TestPasswordValidatorJanOke.java
@@ -1,2 +1,261 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TestPasswordValidatorJanOke {
+
+
+
+    @Test
+    public void testCheckPasswordLengthValid(){
+
+        //GIVEN
+
+        String password = "opjg341opjnpoaw";
+
+        //WHEN
+
+        String expected = "Password is valid";
+
+        String actual = passwordValidatorJanOke.checkPasswordLength(password);
+
+        //THEN
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testCheckPasswordLengthNotValid(){
+
+        //GIVEN
+
+        String password = "opjg";
+
+        //WHEN
+
+        String expected = "Password is too short";
+
+        String actual = passwordValidatorJanOke.checkPasswordLength(password);
+
+        //THEN
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testCheckPasswordContainsDigits(){
+
+        // GIVEN
+
+        String password = "123opasdjg";
+
+        // WHEN
+
+        boolean actual = passwordValidatorJanOke.checkPasswordContainsDigits(password);
+
+        // THEN
+
+        Assertions.assertTrue(actual);
+
+    }
+
+    @Test
+    public void testCheckPasswordDoesntContainsDigits(){
+
+        // GIVEN
+
+        String password = "opasdjg";
+
+        // WHEN
+
+        boolean actual = passwordValidatorJanOke.checkPasswordContainsDigits(password);
+
+        // THEN
+
+        Assertions.assertFalse(actual);
+
+    }
+
+    @Test
+    public void testCheckPasswordContainsUpperAndLowerChars(){
+
+        // GIVEN
+
+        String password = "opaGsDdjg";
+
+        // WHEN
+
+        boolean actual = passwordValidatorJanOke.checkPasswordContainsUpperOrLowerChars(password);
+
+        // THEN
+
+        Assertions.assertTrue(actual);
+
+    }
+
+    @Test
+    public void testCheckPasswordDoesntContainsUpperAndLowerChars(){
+
+        // GIVEN
+
+        String password = "apügkwüp";
+
+        // WHEN
+
+        boolean actual = passwordValidatorJanOke.checkPasswordContainsUpperOrLowerChars(password);
+
+        // THEN
+
+        Assertions.assertFalse(actual);
+
+    }
+
+    @Test
+    public void testCheckPasswordListContainsUpperAndLowerChars(){
+
+        // GIVEN
+
+        String password = "opaGsDdjg";
+
+        // WHEN
+
+        boolean actual = passwordValidatorJanOke.checkPasswordContainsUpperOrLowerChars(password);
+
+        // THEN
+
+        Assertions.assertTrue(actual);
+
+    }
+
+    @Test
+    public void testCheckPasswordListDoesntContainsUpperAndLowerChars(){
+
+        // GIVEN
+
+        String password = "apügkwüp";
+
+        // WHEN
+
+        boolean actual = passwordValidatorJanOke.checkPasswordContainsUpperOrLowerChars(password);
+
+        // THEN
+
+        Assertions.assertFalse(actual);
+
+    }
+
+    @Test
+    public void testCheckPasswordListContainsDigits(){
+
+        // GIVEN
+
+        String[] passwordList = {"np4asdasdnpo","jhasd4asdasdegio","kaoenasdasd4agop"};
+
+        // WHEN
+
+        String expected = passwordList.length + " passwords contain digits.";
+
+        String actual = passwordValidatorJanOke.checkPasswordListContainsDigits(passwordList);
+
+        // THEN
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testCheckPasswordListContainsDigits1True(){
+
+        // GIVEN
+
+        String[] passwordList = {"npasdasdnpo","jhasdasdasdegio","kaoenasdasd4agop"};
+
+        // WHEN
+
+        String expected = "1 password contains digits.";
+
+        String actual = passwordValidatorJanOke.checkPasswordListContainsDigits(passwordList);
+
+        // THEN
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testCheckPasswordListContainsDigitsNonTrue(){
+
+        // GIVEN
+
+        String[] passwordList = {"npasdasdnpo","jhasdasdasdegio","kaoenasdasdagop"};
+
+        // WHEN
+
+        String expected = "no password contains digits.";
+
+        String actual = passwordValidatorJanOke.checkPasswordListContainsDigits(passwordList);
+
+        // THEN
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testCheckPasswordListContainsUpperOrLowerChars2True(){
+
+        // GIVEN
+
+        String[] passwordList = {"npasDdasdnpo","jhasdasddasdegio","kaDoenasdasdagop"};
+
+        // WHEN
+
+        String expected = "2 passwords contain lower and upper case letters.";
+
+        String actual = passwordValidatorJanOke.checkPasswordListContainsUpperOrLowerChars(passwordList);
+
+        // THEN
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void testCheckPasswordListContainsUpperOrLowerCharsFalse(){
+
+        // GIVEN
+
+        String[] passwordList = {"npasdasdnpo","jhasdasddasdegio","kaoenasdasdagop"};
+
+        // WHEN
+
+        String expected = "no password contains lower and upper case letters.";
+
+        String actual = passwordValidatorJanOke.checkPasswordListContainsUpperOrLowerChars(passwordList);
+
+        // THEN
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testCheckPasswordListContainsUpperOrLowerChars3True(){
+
+        // GIVEN
+
+        String[] passwordList = {"npaDsdasdnpo","jhasdasDddasdegio","kaoenaDsdasdagop"};
+
+        // WHEN
+
+        String expected = "3 passwords contain lower and upper case letters.";
+
+        String actual = passwordValidatorJanOke.checkPasswordListContainsUpperOrLowerChars(passwordList);
+
+        // THEN
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
 }
